9cab6088f7d157893c53a0fc6d8f4f309dedea5f
gpmarchi <gustavomarchi@gmail.com>
2024-12-27 13:27:23 -0300

    feat: add tokens package dependency to web project

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,9 @@
-import { Card } from '@repo/ui/card'
+import { colors } from '@repo/tokens/colors'
 
 export default function Home() {
   return (
     <div>
-      <Card title="Hello World" href="/">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tristique
-        lorem ornare nunc consequat, vel sodales arcu faucibus. Morbi eget porta
-        nisi, quis auctor quam. Phasellus venenatis pretium nunc, quis euismod
-        metus fringilla ut. Morbi hendrerit turpis id mi condimentum, id iaculis
-        nulla egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec venenatis metus at fringilla pretium. Sed at lacus at orci
-        dignissim rhoncus a non lorem. Pellentesque id elit sit amet dui iaculis
-        aliquet sed id tortor. Duis at dui quis lorem viverra rhoncus. Praesent
-        pulvinar, nisl vitae gravida tincidunt, purus lectus facilisis augue,
-        vel maximus lectus massa quis nisi.
-      </Card>
+      <pre>{JSON.stringify(colors, null, 2)}</pre>
     </div>
   )
 }
