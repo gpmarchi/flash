1d43ee55003b5bfec6da0a3c9f84f1e0855e32dd
gpmarchi <gustavomarchi@gmail.com>
2025-01-23 10:59:02 -0300

    feat: correctly add flash card animations

--- apps/web/src/app/flash/page.tsx
+++ apps/web/src/app/flash/page.tsx
@@ -43,21 +43,21 @@ export default function Flash() {
             <AnimatePresence>
               {shouldShowAnswer ? (
                 <motion.p
-                  // layoutId="card-text"
+                  key="card-result"
                   className="text-geyser-800 leading-base text-center flex justify-center items-center"
-                  initial={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 2 }}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1, delay: 0.5 }}
                 >
                   .map
                 </motion.p>
               ) : (
                 <motion.p
-                  // layoutId="card-text"
+                  key="card-question"
                   className="text-geyser-800 leading-base text-center flex justify-center items-center"
                   initial={{ opacity: 1 }}
                   exit={{ opacity: 0 }}
-                  transition={{ duration: 2 }}
+                  transition={{ duration: 0.25 }}
                 >
                   Método utilizado para percorrer um array e criar um novo array
                   a partir do original com possíveis modificações.
